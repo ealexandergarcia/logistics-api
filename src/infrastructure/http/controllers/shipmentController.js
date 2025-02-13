1e02e6421f08ced1f641749078e612d1a20f74e5
Edwin <egarciapame@gmail.com>
2025-02-13 18:42:52 -0500

    feat(shipmentController) ✨: add returnAddress handling in registerShipment
    - Validate return address using validateAddress function
    - Create and save return address entity
    - Include returnAddressId in the Shipment entity

--- src/infrastructure/http/controllers/shipmentController.js
+++ src/infrastructure/http/controllers/shipmentController.js
@@ -8,26 +8,34 @@ import { validateAddress } from '../../../services/addressValidationService.js';
 
 export const registerShipment = async (req, res) => {
   try {
-    const { weight, length, width, height, productType, streetAddress, city, department, postalCode, country, details } = req.body;
+    const { weight, length, width, height, productType, streetAddress, city, department, postalCode, country, details, returnStreetAddress, returnCity, returnDepartment, returnPostalCode, returnCountry, returnDetails } = req.body;
     const userId = req.user.userId;
 
     const fullAddress = `${streetAddress}, ${city}, ${country}`;
-    console.log('fullAddress:', fullAddress);
+    const fullReturnAddress = `${returnStreetAddress}, ${returnCity}, ${returnCountry}`;
     
     if (!streetAddress || !(await validateAddress(fullAddress))) {
       return res.status(400).json({ message: 'Invalid destination address' });
     }
 
-    // Create and save the address
+    if (!returnStreetAddress || !(await validateAddress(fullReturnAddress))) {
+      return res.status(400).json({ message: 'Invalid return address' });
+    }
+
+    // Create and save the shipping address
     const address = new Address({ streetAddress, city, department, postalCode, country, details });
     const addressId = await AddressRepository.save(address);
+
+    // Create and save the return address
+    const returnAddress = new Address({ streetAddress: returnStreetAddress, city: returnCity, department: returnDepartment, postalCode: returnPostalCode, country: returnCountry, details: returnDetails });
+    const returnAddressId = await AddressRepository.save(returnAddress);
 
     // Create and save the package
     const pkg = new Package({ weight, length, width, height, productType });
     const packageId = await PackageRepository.save(pkg);
 
     // Create and save the shipment
-    const shipment = new Shipment({ userId, packageId, addressId });
+    const shipment = new Shipment({ userId, packageId, addressId, returnAddressId });
     const shipmentId = await ShipmentRepository.save(shipment);
 
     res.status(201).json({ message: 'Shipment registered successfully!', shipmentId });
