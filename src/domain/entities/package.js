842ddc214b91b5ba7ed8372fbc3046b968acc278
Edwin <egarciapame@gmail.com>
2025-02-13 17:34:13 -0500

    feat(package) ✨ : add Package entity
    - Define the Package entity with weight, length, width, height, and productType fields

---
+++ src/domain/entities/package.js
@@ -0,0 +1,11 @@
+class Package {
+    constructor({ weight, length, width, height, productType }) {
+      this.weight = weight;
+      this.length = length;
+      this.width = width;
+      this.height = height;
+      this.productType = productType;
+    }
+  }
+  
+  export default Package;
