c62509b961db46a58b54ee08c9aaaf4456c079c1
Edwin <egarciapame@gmail.com>
2025-02-13 11:57:59 -0500

    feat(routes) ✨ : add login endpoint for user authentication
    - Adds a POST /users/login endpoint for user login.
    - Validates user credentials and generates a JWT token upon successful login.
    - Includes Swagger documentation for the login endpoint.
    - Ensures that the login endpoint is rate-limited and versioned.

--- src/infrastructure/http/routes/userRoutes.js
+++ src/infrastructure/http/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { registerValidator } from '../validators/userValidator.js';
-import { registerUser } from '../controllers/userController.js';
+import { registerUser, loginUser } from '../controllers/userController.js';
 import { limit } from '../middlewares/rateLimiter.js';
 import { versioning } from '../middlewares/versioning.js';
 import handleValidationErrors from '../middlewares/errorHandler.js';
@@ -50,5 +50,42 @@ const router = express.Router();
  *         description: Internal server error
  */
 router.post(`/register`, limit('post'), versioning('1.0.0'), registerValidator, handleValidationErrors, registerUser);
+
+/**
+ * @swagger
+ * /users/login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: user@example.com
+ *               password:
+ *                 type: string
+ *                 example: yourpassword
+ *     parameters:
+ *       - in: header
+ *         name: x-version
+ *         required: true
+ *         schema:
+ *           type: string
+ *           example: 1.0.0
+ *         description: API version
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       400:
+ *         description: Validation error
+ *       500:
+ *         description: Internal server error
+ */
+router.post(`/login`, limit('post'), versioning('1.0.0'), handleValidationErrors, loginUser);
 
 export default router;
