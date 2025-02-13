320ed5dee8d826ecb1177835ecd717a276c4451a
Edwin <egarciapame@gmail.com>
2025-02-13 11:15:13 -0500

    refactor(app) ♻️ : mount multiple route files in app
    - Updates app.js to mount user, routes.
    - Ensures that the application can handle multiple base paths.

--- src/config/app.js
+++ src/config/app.js
@@ -2,26 +2,19 @@ import express from 'express';
 import rateLimit from 'express-rate-limit';
 import swaggerUi from 'swagger-ui-express';
 import errorHandler from '../infrastructure/http/middlewares/errorHandler.js';
+import userRoutes from '../infrastructure/http/routes/userRoutes.js';
+import swaggerSpecs from '../swagger/swagger.js';
 
 export const createApp = () => {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+
+  app.use("/api/v1/users", userRoutes);
   app.use(errorHandler);
-
-  const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100
-  });
-  app.use(limiter);
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-  });
 
   return app;
 };
