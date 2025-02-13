adeecd5a0aecfe38b59afd75dbb2cb0f6df81a83
Edwin <egarciapame@gmail.com>
2025-02-13 11:17:55 -0500

    feat(use-case) ✨ : add LoginUserUseCase
    - Adds LoginUserUseCase to handle user login logic.
    - Validates user credentials and generates a JWT token if valid.
    - Ensures that the use case can be used to authenticate users.

---
+++ src/application/use-cases/loginUserUseCase.js
@@ -0,0 +1,23 @@
+import bcrypt from 'bcrypt';
+import JwtService from '../../infrastructure/http/middlewares/jwtService.js';
+import UserRepository from '../../domain/repositories/userRepository.js';
+
+class LoginUserUseCase {
+  async execute({ email, password }) {
+    const user = await UserRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid password");
+    }
+
+    const token = JwtService.generateToken({ userId: user.id });
+    return token;
+  }
+}
+
+export { LoginUserUseCase };
