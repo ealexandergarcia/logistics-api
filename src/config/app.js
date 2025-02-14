2c398d768fb92037f31d11255d24647f814dda41
Edwin <egarciapame@gmail.com>
2025-02-13 22:08:35 -0500

    fix(app) 🐛 : add global error handling middleware
    - Add global error handling middleware to handle unexpected errors.
    - Ensure all routes and middlewares are correctly set up.

--- src/config/app.js
+++ src/config/app.js
@@ -1,22 +1,21 @@
 import express from 'express';
 import rateLimit from 'express-rate-limit';
 import swaggerUi from 'swagger-ui-express';
-import errorHandler from '../infrastructure/http/middlewares/errorHandler.js';
+import { errorHandler } from '../infrastructure/http/middlewares/errorHandler.js';
 import userRoutes from '../infrastructure/http/routes/userRoutes.js';
 import shipmentRoutes from '../infrastructure/http/routes/shipmentRoutes.js';
 import swaggerSpecs from '../swagger/swagger.js';
 
-export const createApp = () => {
-  const app = express();
+const app = express();
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
-  app.use("/api/v1/users", userRoutes);
-  app.use('/api/v1/shipments', shipmentRoutes);
-  app.use(errorHandler);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/shipments', shipmentRoutes);
 
-  return app;
-};
+app.use(errorHandler);
+
+export const createApp = () => app;
