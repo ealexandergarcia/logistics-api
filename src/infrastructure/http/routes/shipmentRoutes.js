d8db6ab7c18518b47e55d01ae6585ef33be784c2
Edwin <egarciapame@gmail.com>
2025-02-13 22:08:09 -0500

    fix(shipmentRoutes) 🐛 : correct import and usage of middlewares
    - Ensure all middlewares are correctly imported and used in shipment routes.

--- src/infrastructure/http/routes/shipmentRoutes.js
+++ src/infrastructure/http/routes/shipmentRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import { registerShipment } from '../controllers/shipmentController.js';
+import { shipmentValidator } from '../validators/shipmentValidator.js';
+import { handleValidationErrors } from '../middlewares/errorHandler.js';
 import { jwtMiddleware } from '../middlewares/authMiddleware.js';
 import { limit } from '../middlewares/rateLimiter.js';
 import { versioning } from '../middlewares/versioning.js';
-import handleValidationErrors from '../middlewares/errorHandler.js';
-import { shipmentValidator } from '../validators/shipmentValidator.js';
 
 const router = express.Router();
 
