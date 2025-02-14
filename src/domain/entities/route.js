41b5df53f0f0f35efde2fd89bda02f49c83e5907
Edwin <egarciapame@gmail.com>
2025-02-14 00:39:35 -0500

    feat(route) ✨ : add Route entity
    - Define Route entity with properties id, name, startLocation, endLocation, and estimatedDuration.

---
+++ src/domain/entities/route.js
@@ -0,0 +1,11 @@
+class Route {
+    constructor(id, name, startLocation, endLocation, estimatedDuration) {
+      this.id = id;
+      this.name = name;
+      this.startLocation = startLocation;
+      this.endLocation = endLocation;
+      this.estimatedDuration = estimatedDuration;
+    }
+  }
+  
+  export default Route;
