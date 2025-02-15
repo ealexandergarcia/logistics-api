572520014bc5c620b6a34e46d69ac83e7cdcf64e
Edwin <egarciapame@gmail.com>
2025-02-15 12:08:28 -0500

    feat(shipmentController) ✨: change of error message

--- src/infrastructure/http/controllers/shipmentController.js
+++ src/infrastructure/http/controllers/shipmentController.js
@@ -67,6 +67,6 @@ export const assignShipment = async (req, res, next) => {
 
     res.status(200).json({ message: 'Shipment assigned successfully', shipment });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: 'Internal server error' });
   }
 };
